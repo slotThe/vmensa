06a1f118d9df13d9a5eb25a93f559f722b5f83df
Tony Zorman <tonyzorman@mailbox.org>
2020-06-22 11:36:07 +0200

    Handle concurrency a bit better

--- src/Main.hs
+++ src/Main.hs
@@ -43,35 +43,35 @@ main = do
     -- Get specified date in YYYY-MM-DD format, then build all canteens and
     -- decide in which order they will be printed.
     d <- getDate date
-    let canteens = map ($! d) [alte, uboot, zelt, siedepunkt]
+    let canteens = map ($ d) [alte, uboot, zelt, siedepunkt]
 
-    {- Create new manager for handling network connections, then connect to the API
-       and parse the necessary JSON.  Note that 'mapConcurrently' creates a
-       thread for every item of 'canteens', though since that list is small this
-       is not an issue for us.
+    {- Create new manager for handling network connections, then asynchronously
+       connect to the API and parse the necessary JSON.  Note that
+       'mapConcurrently' creates a thread for every item of 'canteens', though
+       since that list is small this is not an issue for us.
     -}
     manager <- newManager tlsManagerSettings
-    mensen  <- mapConcurrently (getMensa manager opts) canteens
+    let prettyMensa :: Mensa -> IO Text
+        prettyMensa m = mprint lineWrap (tshow date) <$> getMensa manager opts m
+    mensen <- mapConcurrently prettyMensa canteens
 
-    -- Print out the results.
-    traverse_ (mprint lineWrap (tshow date)) mensen
-
+    -- Print out the results synchronously.
+    traverse_ T.putStr mensen
   where
     -- | Pretty print a 'Mensa'.
     mprint
         :: Int    -- ^ Line wrap
         -> Text   -- ^ Day when the meals are offered
         -> Mensa
-        -> IO ()
-    mprint lw d mensa@Mensa{ name, meals } =
-        if empty mensa
-            then pure ()
-            else traverse_ T.putStrLn
-                     [ separator
-                     , d <> " in: " <> name
-                     , separator
-                     , showMeals lw meals
-                     ]
+        -> Text
+    mprint lw d mensa@Mensa{ name, meals }
+        | empty mensa = ""
+        | otherwise   = T.unlines
+            [ separator
+            , d <> " in: " <> name
+            , separator
+            , showMeals lw meals
+            ]
 
     -- | Separator for visual separation of different canteens.
     separator :: Text
